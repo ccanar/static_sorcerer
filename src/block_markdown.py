57ca3a1c2d89d6b462614418e610863d0d3169f4
ccanar <150935498+ccanar@users.noreply.github.com>
2025-03-24 21:59:52 +0100

    Add block type
    Add markdown to block type
    Add tests for block type
    add tests for markdown to block type

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,15 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = ("paragraph",)
+    HEADING = ("heading",)
+    CODE = ("code",)
+    QUOTE = ("quote",)
+    UNORDERED_LIST = ("unordered_list",)
+    ORDERED_LIST = ("ordered_list",)
+
+
 def markdown_to_blocks(markdown_text: str) -> list[str]:
     output: list[str] = []
     split_text = markdown_text.split("\n\n")
@@ -5,3 +17,46 @@ def markdown_to_blocks(markdown_text: str) -> list[str]:
         if len(element) > 0:
             output.append(element.strip())
     return output
+
+
+def block_to_block_type(markdown_text: str) -> BlockType:
+    if markdown_text == "":
+        raise Exception("block_to_block_type EMPTY STRING INPUT")
+    if markdown_text[0] == " ":
+        raise Exception("block_to_block_type STARTS WITH A SPACE")
+    if markdown_text.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    elif markdown_text.startswith("```") and markdown_text.endswith("```"):
+        return BlockType.CODE
+
+    line_list = markdown_text.splitlines()
+    if markdown_text.startswith(">"):
+        all_lines_proper_start = True
+        for line in line_list:
+            if not line.startswith(">"):
+                all_lines_proper_start = False
+                break
+        if all_lines_proper_start:
+            return BlockType.QUOTE
+
+    if markdown_text.startswith("- "):
+        all_lines_proper_start = True
+        for line in line_list:
+            if not line.startswith("- "):
+                all_lines_proper_start = False
+                break
+        if all_lines_proper_start:
+            return BlockType.UNORDERED_LIST
+
+    if markdown_text.startswith("1. "):
+        number = 1
+        all_lines_proper_start = True
+        for line in line_list:
+            if not line.startswith(f"{number}. "):
+                all_lines_proper_start = False
+                break
+            number += 1
+        if all_lines_proper_start:
+            return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import BlockType, markdown_to_blocks, block_to_block_type
 
 
 class test_block_markdown(unittest.TestCase):
@@ -53,6 +53,116 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_block_type_heading(self):
+        text1 = "# One"
+        text2 = "## Two"
+        text3 = "### Three"
+        text4 = "#### Four"
+        text5 = "##### Five"
+        text6 = "###### Six"
+        text7 = "####### Seven"
+        text8 = "#Nospace"
+        text9 = "Nothing"
+
+        self.assertEqual(block_to_block_type(text1), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(text2), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(text3), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(text4), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(text5), BlockType.HEADING)
+        self.assertEqual(block_to_block_type(text6), BlockType.HEADING)
+        self.assertNotEqual(block_to_block_type(text7), BlockType.HEADING)
+        self.assertNotEqual(block_to_block_type(text8), BlockType.HEADING)
+        self.assertNotEqual(block_to_block_type(text9), BlockType.HEADING)
+
+    def test_block_to_block_type_code(self):
+        text1 = "```code```"
+        text2 = "```code\nsome more\nand even more```"
+        text3 = "```not code"
+        text4 = "not code 2```"
+        text5 = "``NOT```"
+        text6 = "```NOT2`"
+        text7 = "Just text"
+        text8 = "# Just heading"
+
+        self.assertEqual(block_to_block_type(text1), BlockType.CODE)
+        self.assertEqual(block_to_block_type(text2), BlockType.CODE)
+        self.assertNotEqual(block_to_block_type(text3), BlockType.CODE)
+        self.assertNotEqual(block_to_block_type(text4), BlockType.CODE)
+        self.assertNotEqual(block_to_block_type(text5), BlockType.CODE)
+        self.assertNotEqual(block_to_block_type(text6), BlockType.CODE)
+        self.assertNotEqual(block_to_block_type(text7), BlockType.CODE)
+        self.assertNotEqual(block_to_block_type(text8), BlockType.CODE)
+
+    def test_block_to_block_type_quote(self):
+        text1 = "123"
+        text2 = "123 "
+        text3 = "Just text"
+        text4 = "# a heading"
+        text5 = "```here, have some code```"
+        text6 = "1. One\n2. Two\n3. Three"
+        text7 = ">Just one line"
+        text8 = "> Another line"
+        text9 = """>A line
+>Another one
+>And one more"""
+        text10 = """>Starts good
+Ends bad"""
+        text11 = """Starts bad
+>good ending"""
+
+        self.assertNotEqual(block_to_block_type(text1), BlockType.QUOTE)
+        self.assertNotEqual(block_to_block_type(text2), BlockType.QUOTE)
+        self.assertNotEqual(block_to_block_type(text3), BlockType.QUOTE)
+        self.assertNotEqual(block_to_block_type(text4), BlockType.QUOTE)
+        self.assertNotEqual(block_to_block_type(text5), BlockType.QUOTE)
+        self.assertNotEqual(block_to_block_type(text6), BlockType.QUOTE)
+        self.assertEqual(block_to_block_type(text7), BlockType.QUOTE)
+        self.assertEqual(block_to_block_type(text8), BlockType.QUOTE)
+        self.assertEqual(block_to_block_type(text9), BlockType.QUOTE)
+        self.assertNotEqual(block_to_block_type(text10), BlockType.QUOTE)
+        self.assertNotEqual(block_to_block_type(text11), BlockType.QUOTE)
+
+    def test_block_to_block_type_unordered_list(self):
+        text1 = "123"
+        text2 = "123 "
+        text3 = "Just text"
+        text4 = "# a heading"
+        text5 = "```here, have some code```"
+        text6 = "1. One\n2. Two\n3. Three"
+
+        self.assertNotEqual(block_to_block_type(text1), BlockType.UNORDERED_LIST)
+        self.assertNotEqual(block_to_block_type(text2), BlockType.UNORDERED_LIST)
+        self.assertNotEqual(block_to_block_type(text3), BlockType.UNORDERED_LIST)
+        self.assertNotEqual(block_to_block_type(text4), BlockType.UNORDERED_LIST)
+        self.assertNotEqual(block_to_block_type(text5), BlockType.UNORDERED_LIST)
+        self.assertNotEqual(block_to_block_type(text6), BlockType.UNORDERED_LIST)
+
+    def test_block_to_block_type_ordered_list(self):
+        text1 = "123"
+        text2 = "123 "
+        text3 = "Just text"
+        text4 = "# a heading"
+        text5 = "```here, have some code```"
+
+        self.assertNotEqual(block_to_block_type(text1), BlockType.ORDERED_LIST)
+        self.assertNotEqual(block_to_block_type(text2), BlockType.ORDERED_LIST)
+        self.assertNotEqual(block_to_block_type(text3), BlockType.ORDERED_LIST)
+        self.assertNotEqual(block_to_block_type(text4), BlockType.ORDERED_LIST)
+        self.assertNotEqual(block_to_block_type(text5), BlockType.ORDERED_LIST)
+
+    def test_block_to_block_type_paragraph(self):
+        text1 = "1. Smile"
+        text2 = "- 123"
+        text3 = "# a heading"
+        text4 = "```here, have some code```"
+        text5 = "1. One\n2. Two\n3. Three"
+
+        self.assertNotEqual(block_to_block_type(text1), BlockType.PARAGRAPH)
+        self.assertNotEqual(block_to_block_type(text2), BlockType.PARAGRAPH)
+        self.assertNotEqual(block_to_block_type(text3), BlockType.PARAGRAPH)
+        self.assertNotEqual(block_to_block_type(text4), BlockType.PARAGRAPH)
+        self.assertNotEqual(block_to_block_type(text5), BlockType.PARAGRAPH)
 
 
 if __name__ == "__main__":
