ec87f75a6bf1b404c7c9f47238398c45ace76352
ccanar <150935498+ccanar@users.noreply.github.com>
2025-03-27 21:39:28 +0100

    Build page for production

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import os
 import shutil
+import sys
 
-from block_markdown import markdown_to_html_node, extract_title
+from block_markdown import extract_title, markdown_to_html_node
+
+
 # rm -rf public/*
 # cp -r static/* public/
 def copy_content(source, destination):
@@ -15,10 +18,13 @@ def copy_content(source, destination):
         elif os.path.isdir(source_item_path):
             print(f"mkdir {destination_item_path}")
             os.mkdir(destination_item_path)
-            print(f"{destination_item_path} exists: {os.path.exists(destination_item_path)}")
+            print(
+                f"{destination_item_path} exists: {os.path.exists(destination_item_path)}"
+            )
             copy_content(source_item_path, destination_item_path)
 
-def generate_page(from_path, template_path, dest_path):
+
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as markdown_file:
         markdown_file_data = markdown_file.read()
@@ -28,49 +34,68 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_file_data)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html_string)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
     if not os.path.exists(dest_path):
         os.mknod(dest_path)
     with open(dest_path, "w") as destination:
         destination.write(template)
 
-def generate_pages_recursively(content_directory_path, template_path, destination_directory_path):
+
+def generate_pages_recursively(
+    content_directory_path, template_path, destination_directory_path, basepath
+):
     list_dir_content = os.listdir(content_directory_path)
     for item in list_dir_content:
         content_item_path = os.path.join(content_directory_path, item)
         destination_item_path = os.path.join(destination_directory_path, item)
         destination_item_path = destination_item_path.replace(".md", ".html")
         if os.path.isfile(content_item_path) and content_item_path.endswith(".md"):
-            print(f"Generated {content_item_path} using {template_path} as {destination_item_path}")
-            generate_page(content_item_path, template_path, destination_item_path)
+            print(
+                f"Generated {content_item_path} using {template_path} as {destination_item_path}"
+            )
+            generate_page(
+                content_item_path, template_path, destination_item_path, basepath
+            )
         elif os.path.isdir(content_item_path):
             print(f"mkdir {destination_item_path}")
             os.mkdir(destination_item_path)
-            print(f"{destination_item_path} exists: {os.path.exists(destination_item_path)}")
-            generate_pages_recursively(content_item_path, template_path, destination_item_path)
+            print(
+                f"{destination_item_path} exists: {os.path.exists(destination_item_path)}"
+            )
+            generate_pages_recursively(
+                content_item_path, template_path, destination_item_path, basepath
+            )
+
 
 def main():
+    if len(sys.argv) <= 1:
+        basepath = "/"
+    else:
+        basepath = sys.argv[1]
+
+    static_path = "static/"
     source_path = "content/"
     template_path = "template.html"
-    destination_path = "public/"
+    destination_path = "docs/"
 
-    if os.path.exists("public/"):
-        print("public/ exists\rmtree public/")
-        shutil.rmtree("public/")
-    if not os.path.exists("public/"):
-        print("public/ directory does not exists, mkdir public/")
-        os.mkdir("public/")
-        if os.path.exists("public/"):
-            print("public/ exists")
-            copy_content("static/", "public/")
-            if os.listdir("static/") == os.listdir("public/"):
-                print("---Copy from static/ to public/ successful---")
-            print("ls static/", os.listdir("static/"))
-            print("ls public/", os.listdir("public/"))
+    if os.path.exists(destination_path):
+        print(f"{destination_path} exists\rmtree {destination_path}")
+        shutil.rmtree(destination_path)
+    if not os.path.exists(destination_path):
+        print(f"{destination_path} directory does not exists, mkdir {destination_path}")
+        os.mkdir(destination_path)
+        if os.path.exists(destination_path):
+            print(f"{destination_path} exists")
+            copy_content(static_path, destination_path)
+            if os.listdir(static_path) == os.listdir(destination_path):
+                print(f"---Copy from static/ to {destination_path} successful---")
+            print("ls static/", os.listdir(static_path))
+            print(f"ls {destination_path}", os.listdir(destination_path))
     print("Hello, World!")
 
-    generate_pages_recursively(source_path, template_path, destination_path)
+    generate_pages_recursively(source_path, template_path, destination_path, basepath)
 
 
 if __name__ == "__main__":
     main()
-
